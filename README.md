a38b71d2058073e491d42a7f3d7077f2c6fa8603
simon-tanna <gcab022109@coderacademy.edu.au>
2021-11-19 10:36:32 +1000

    added constants to day 1 file. created day 2 file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # This is the week 6 material
+
+In the ruby_prac.rb file. comment out parts of the program to view different sections in isolation
--- ruby_day1_1.rb
+++ ruby_day1_1.rb
@@ -1,17 +1,29 @@
+# Normally constant variables are defined at the start of the program
+# Define current year constant
+CURRENT_YEAR = 2021
+# Defines creator
+CREATOR = "simon"
 # Prints welcome message and asks for name
 print ("Welcome human, let's get to know each other. ")
 puts ("What is your name? ")
 # gets the input from the terminal and stores in the name variable
 # chomp removes the \n key from the variable
 name = gets.chomp
-# print the message with the value we got from the user
-puts ("Nice to meet you, #{name}")
+# checks if the user is the creator, downcase not to make it case sensitive
+if name.downcase == CREATOR
+    puts ("Welcome my lord and master. I am at your service")
+    puts ("I feel like a durry bruss")
+else
+    # print the message with the value we got from the user
+    puts ("Nice to meet you, #{name}")
+    puts ("I'm a happy litte program")
+end
 # here the bot asks for a name for itself
 print ("I dont't have a name yet, can you please give me one? ")
 # gets the input from the terminal and stores bot_name
 bot_name = gets.chomp
 # The execution sleeps for 3 seconds, simulating that the bot is thinking/typing
-sleep (3)
+# sleep (3)
 # prints bot's message
 puts ("Thank you #{name}! My name is now #{bot_name}")
 # asks the user how old they are
@@ -19,12 +31,12 @@ puts ("How old are you?")
 # gets the input of the age and stores the variable
 age = gets.chomp.to_i
 # calculates the year of birth
-year = 2021 - age
+year = CURRENT_YEAR - age
 # prints the user's age
 puts ("#{name}, you are #{age} years old")
 # prints the user's year of birth
-puts ("calculating....")
-sleep (2)
+# puts ("calculating....")
+# sleep (2)
 puts ("You were born in #{year}")
 # if conditions
     # the actions taken if the condition is true
@@ -35,8 +47,7 @@ if age >= 18
 else
     puts ("#{name}, you are a child")
 end
-# asks user if they like butter chicken
-puts ("Do you like to eat butter chicken?")
+
 
 
 
